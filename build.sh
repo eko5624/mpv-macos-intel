1c383825432242fdb680cd0ed4fe675090a57f78
eko5624 <562433403@qq.com>
2023-02-04 00:00:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ if [[ ("$OSTYPE" == "darwin"*) ]]; then
     export MACOSX_DEPLOYMENT_TARGET=11.0
     MACOS_M1=true
   else
-    export MACOSX_DEPLOYMENT_TARGET=10.13
+    export MACOSX_DEPLOYMENT_TARGET=10.14
   fi
 fi
 
