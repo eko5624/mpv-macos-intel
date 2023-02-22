95273c4925b803afdc273711924ffd0e35bf4e5d
eko5624 <562433403@qq.com>
2023-02-22 18:31:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -472,6 +472,7 @@ if build "libplacebo" "master"; then
   cd $PACKAGES
   git clone --recursive https://code.videolan.org/videolan/libplacebo
   cd libplacebo
+  execute meson setup build \
     --prefix="${WORKSPACE}" \
     --buildtype=release \
     -Dvulkan=disabled \
