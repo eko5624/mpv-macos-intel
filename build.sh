cd774af44a293dbcbe5c5d7d147188d0161e344c
eko5624 <562433403@qq.com>
2023-03-02 20:10:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [[ ("$OSTYPE" == "darwin"*) ]]; then
     export MACOSX_DEPLOYMENT_TARGET=11
     MACOS_M1=true
   else
-    export MACOSX_DEPLOYMENT_TARGET=10.11
+    export MACOSX_DEPLOYMENT_TARGET=10.13
   fi
 fi
 
@@ -1372,7 +1372,7 @@ if build "mpv" "master"; then
     -Dprefix="${WORKSPACE}" \
     -Dmacos-media-player=disabled \
     -Dmanpage-build=disabled \
-    -Dswift-flags="-target x86_64-apple-macos10.11"
+    -Dswift-flags="-target x86_64-apple-macos10.13"
   meson compile -C build
   
   # fix can't find libvpx.8.dylib 
