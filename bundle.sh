c14c982961f14f0f1b597c4ca206e6b4fb36f415
eko5624 <562433403@qq.com>
2023-03-15 19:26:39 +0800

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,6 @@
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PACKAGES=$DIR/packages
 WORKSPACE=$DIR/workspace
-RUNNER_WORKSPACE=/Users/runner/work/mpv-macos-intel/mpv-macos-intel/workspace
 SWIFT_PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/lib/swift/macosx
 
 #copy all *.dylib to mpv.app
@@ -32,7 +31,7 @@ for dylib in "${mpv_otool[@]}"; do
   mpv_dylibs_otool+=("${mpv_dylib_otool[@]}")
 done
 mpv_dylibs_otool=($(echo "${mpv_dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "${mpv_dylibs_otool[@]}" > $DIR/mpv_dylibs_otool
+echo "${mpv_dylibs_otool[@]}" > $PACKAGES/mpv/build/mpv_dylibs_otool
 
 dylibs_otool=()
 for dylib in "${mpv_dylibs_otool[@]}"; do
@@ -40,7 +39,15 @@ for dylib in "${mpv_dylibs_otool[@]}"; do
   dylibs_otool+=("${dylib_otool[@]}")
 done	
 dylibs_otool=($(echo "${dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "${dylibs_otool[@]}" > $DIR/dylibs_otool
+
+for dylib in "${dylibs_otool[@]}"; do
+  dylib_dylib_otool=($(otool -L $dylib | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
+  if [[ "${#dylib_dylib_otool[@]}" > 1 ]]; then
+    dylibs_otool+=("${dylib_dylib_otool[@]}")
+  fi  
+done	
+dylibs_otool=($(echo "${dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+echo "${dylibs_otool[@]}" > $PACKAGES/mpv/build/dylibs_otool
 
 all_dylibs=(${mpv_otool[@]} ${swift_dylibs_otool[@]} ${mpv_dylibs_otool[@]} ${dylibs_otool[@]})
 all_dylibs=($(echo "${all_dylibs[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
@@ -54,28 +61,25 @@ for f in "${all_dylibs[@]}"; do
   fi  
 done
 
-#removing swift rpath definitions towards dev tools
-rpaths_swift=($(otool -l $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep -A2 LC_RPATH | grep path | grep -E "Xcode|CommandLineTools|swift" | awk '{ print $2 }'))
-for f in "${rpaths_swift[@]}"; do
-  install_name_tool -delete_rpath $f $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
+#removing rpath definitions towards dev tools
+rpaths=($(otool -l $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep -A2 LC_RPATH | grep path | awk '{ print $2 }'))
+for f in "${rpaths[@]}"; do
+  sudo install_name_tool -delete_rpath $f $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
 done
-
-#removing lib rpath
-install_name_tool -delete_rpath $RUNNER_WORKSPACE/lib $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
 
 #setting additional rpath for swift libraries
 install_name_tool -add_rpath @executable_path/lib $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
 
 for dylib in "${mpv_otool[@]}"; do
-  install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
+  sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
 done
 
 for f in $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/*.dylib; do
   if [[ "$(basename $f)" != "libswift"* ]]; then
-    install_name_tool -id "@executable_path/lib/$(basename $f)" "$PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)"
+    sudo install_name_tool -id "@executable_path/lib/$(basename $f)" "$PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)"
     dylib_tool=($(otool -L $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f) | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
     for dylib in "${dylib_tool[@]}"; do
-      install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)
+      sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)
     done
   fi   
 done
