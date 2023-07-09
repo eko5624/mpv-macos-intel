33e48b06c2367f149f37980aa94c5fbac4694690
eko5624 <562433403@qq.com>
2023-07-09 15:23:19 +0800

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -2,27 +2,31 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-#copy all *.dylib to mpv.app
 sudo cp -r $DIR/TOOLS/osxbundle/mpv.app $DIR/build
 sudo cp $DIR/build/mpv $DIR/build/mpv.app/Contents/MacOS
 pushd $DIR/build/mpv.app/Contents/MacOS
 sudo ln -s mpv mpv-bundle
 popd
 
-mpv_otool=($(otool -L $DIR/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
-echo "${mpv_otool[@]}" > $DIR/build/mpv_otool.txt
+mpv_deps=($(otool -L $DIR/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }' | tr ' ' '\n'))
+echo "${mpv_deps[@]}" > $DIR/build/mpv_deps.txt
 
 get_deps() {
-  local deps=$(otool -L $1 | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }')
+  local deps=$(otool -L $1 | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk 'NR>1 { print $1 }')
   for dep in $deps; do
     echo $dep
     get_deps $dep
   done
 }
-mpv_deps=$(get_deps "$DIR/build/mpv.app/Contents/MacOS/mpv" | sort -u)
-echo "${mpv_deps[@]}" > $DIR/build/mpv_deps.txt
 
-for f in "${mpv_deps[@]}"; do
+lib_deps=$(get_deps "$DIR/build/mpv.app/Contents/MacOS/mpv" | sort -u)
+echo "${lib_deps[@]}" > $DIR/build/lib_deps.txt
+
+all_deps=(${mpv_deps[@]} ${lib_deps[@]})
+all_deps=($(echo "${all_deps[@]}" | sort -u))
+echo "${all_deps[@]}" > $DIR/build/all_deps.txt
+
+for f in "${all_deps[@]}"; do
   if [[ "$f" != "@loader_path"* ]]; then
     sudo cp $f $DIR/build/mpv.app/Contents/MacOS/lib
   fi
@@ -37,7 +41,7 @@ done
 #setting additional rpath for swift libraries
 sudo install_name_tool -add_rpath @executable_path/lib $DIR/build/mpv.app/Contents/MacOS/mpv
 
-for dylib in "${mpv_otool[@]}"; do
+for dylib in "${mpv_deps[@]}"; do
   sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $DIR/build/mpv.app/Contents/MacOS/mpv
 done
 
