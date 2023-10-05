7ee50ba1044798c9f21984943c1e4c049cb23723
eko5624 <562433403@qq.com>
2023-10-05 22:49:12 +0800

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -1,83 +1,72 @@
 #!/bin/bash
 set -x
 
+#NOTE: put bundle.sh into the parent directory of mpv source code.
+
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-PACKAGES=$DIR/packages
-WORKSPACE=$DIR/workspace
-SWIFT_PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/lib/swift/macosx
+DEP_PATH_PREFIX=/usr/local
 
-
-#copy all *.dylib to mpv.app
-cp -r $PACKAGES/mpv/TOOLS/osxbundle/mpv.app $PACKAGES/mpv/build
-cp $PACKAGES/mpv/build/mpv $PACKAGES/mpv/build/mpv.app/Contents/MacOS
-pushd $PACKAGES/mpv/build/mpv.app/Contents/MacOS
-ln -s mpv mpv-bundle
+sudo cp -r $DIR/mpv/TOOLS/osxbundle/mpv.app $DIR/mpv/build
+sudo cp $DIR/mpv/build/mpv $DIR/mpv/build/mpv.app/Contents/MacOS
+pushd $DIR/mpv/build/mpv.app/Contents/MacOS
+sudo ln -s mpv mpv-bundle
 popd
 
-mpv_deps=($(otool -L $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
+mpv_deps=($(otool -L $DIR/mpv/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }' | xargs basename | grep -Ev "libluajit|libarchive" | sed 's#^#/usr/local/lib/#'))
 for i in "${mpv_deps[@]}"; do
-  echo $i >> $PACKAGES/mpv/build/mpv_deps.txt
+  echo $i >> $DIR/mpv/build/mpv_deps.txt
 done
 
 get_deps() {
-  local deps=$(otool -L $1 | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk 'NR>1 {print $1}')
+  local deps=$(otool -L $1 | grep -e '\t' | grep -Ev "\/usr\/lib|\/System" | awk 'NR>1 {print $1}' | xargs basename | grep -Ev "libluajit|libarchive" | sed 's#^#/usr/local/lib/#')
   for dep in $deps; do
     echo $dep
     get_deps $dep
   done
 }
 
-first_libdeps=($(get_deps $(otool -L $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk 'NR==1 { print $1 }') | sort -u))
-others_libdeps=($(get_deps "$PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv" | sort -u))
+first_libdeps=($(get_deps $(otool -L $DIR/mpv/build/mpv.app/Contents/MacOS/mpv | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk 'NR==1 { print $1 }' | xargs basename | grep -Ev "libluajit" | sed 's#^#/usr/local/lib/#') | sort -u))
+others_libdeps=($(get_deps "$DIR/mpv/build/mpv.app/Contents/MacOS/mpv" | sort -u))
 libdeps=($(echo ${first_libdeps[@]} ${others_libdeps[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '))
 for i in "${libdeps[@]}"; do
-  echo $i >> $PACKAGES/mpv/build/libdeps.txt
+  echo $i >> $DIR/mpv/build/libdeps.txt
 done
 
-mpv_rpath=($(otool -L $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep '@rpath' | awk '{ print $1 }' | awk -F '/' '{print $NF}'))
-for dylib in "${mpv_rpath[@]}"; do
-  swift_dep=($(otool -L $SWIFT_PATH/$dylib | grep '@rpath' | awk '{ print $1 }' | awk -F '/' '{print $NF}'))
-  swift_deps+=("${swift_dep[@]}")
-done
-swift_deps=($(echo "${swift_deps[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-for i in "${swift_deps[@]}"; do
-  echo $i >> $PACKAGES/mpv/build/swift_deps.txt
-done
-
-all_deps=($(echo ${libdeps[@]} ${swift_deps[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '))
+all_deps=($(echo ${mpv_deps[@]} ${libdeps[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '))
 for i in "${all_deps[@]}"; do
-  echo $i >> $PACKAGES/mpv/build/all_deps.txt
+  echo $i >> $DIR/mpv/build/all_deps.txt
 done
 
+#copy all *.dylib to "mpv.app/Contents/MacOS/lib"
 for f in "${all_deps[@]}"; do
-  if [[ "$(basename $f)" != "libswift"* ]]; then
-    find $WORKSPACE/lib -name "$(basename $f)" -print0 | xargs -0 -I {} cp {} $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib
-  else  
-    find $SWIFT_PATH -name "$(basename $f)" -print0 | xargs -0 -I {} cp {} $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib
-  fi  
+  if [[ "$f" = "@loader_path"* ]] || [[ "$f" = "@rpath"* ]]; then
+    find /usr/local -name "$(basename $f)" -print0 | xargs -0 -I {} sudo cp -f {} $DIR/mpv/build/mpv.app/Contents/MacOS/lib
+  elif [[ "$(basename $f)" = "libluajit"* ]]; then
+    sudo cp `brew --prefix luajit`/lib/$(basename $f) $DIR/mpv/build/mpv.app/Contents/MacOS/lib
+  else
+    sudo cp $f $DIR/mpv/build/mpv.app/Contents/MacOS/lib
+  fi
 done
 
 #removing rpath definitions towards dev tools
-rpaths=($(otool -l $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv | grep -A2 LC_RPATH | grep path | awk '{ print $2 }'))
+rpaths=($(otool -l $DIR/mpv/build/mpv.app/Contents/MacOS/mpv | grep -A2 LC_RPATH | grep path | awk '{ print $2 }'))
 for f in "${rpaths[@]}"; do
-  sudo install_name_tool -delete_rpath $f $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
+  sudo install_name_tool -delete_rpath $f $DIR/mpv/build/mpv.app/Contents/MacOS/mpv
 done
 
 #setting additional rpath for swift libraries
-install_name_tool -add_rpath @executable_path/lib $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
+sudo install_name_tool -add_rpath @executable_path/lib $DIR/mpv/build/mpv.app/Contents/MacOS/mpv
 
 for dylib in "${mpv_deps[@]}"; do
-  sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/mpv
+  sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $DIR/mpv/build/mpv.app/Contents/MacOS/mpv
 done
 
-for f in $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/*.dylib; do
-  if [[ "$(basename $f)" != "libswift"* ]]; then
-    sudo install_name_tool -id "@executable_path/lib/$(basename $f)" "$PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)"
-    dylib_tool=($(otool -L $f | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
-    for dylib in "${dylib_tool[@]}"; do
-      if [[ "${#dylib_tool[@]}" > 1 ]]; then
-        sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $PACKAGES/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)
-      fi  
-    done
-  fi   
+for f in $DIR/mpv/build/mpv.app/Contents/MacOS/lib/*.dylib; do
+  sudo install_name_tool -id "@executable_path/lib/$(basename $f)" "$DIR/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)"
+  dylib_tool=($(otool -L $f | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
+  for dylib in "${dylib_tool[@]}"; do
+    if [[ "${#dylib_tool[@]}" > 1 ]]; then
+      sudo install_name_tool -change $dylib @executable_path/lib/$(basename $dylib) $DIR/mpv/build/mpv.app/Contents/MacOS/lib/$(basename $f)
+    fi  
+  done 
 done
