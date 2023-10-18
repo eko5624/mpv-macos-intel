0490b9eeed1b49239a5fef2d4c471d2a61cd7359
eko5624 <562433403@qq.com>
2023-10-18 15:33:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1594,7 +1594,7 @@ if build "mpv" "master"; then
     -Dprefix="${WORKSPACE}" \
     -Dmanpage-build=disabled
 #    -Dswift-flags="-target x86_64-apple-macos10.15"
-  meson compile -C build
+  meson compile -C build --tags runtime,devel
   
   # get latest commit sha
   short_sha=$(git rev-parse --short HEAD)
