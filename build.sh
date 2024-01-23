facabfc5f2200350b9c640f46e64057e0eb24b2b
eko5624 <562433403@qq.com>
2024-01-23 23:51:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -960,11 +960,11 @@ if build "libjxl" "main"; then
   cd $PACKAGES
   git clone https://github.com/libjxl/libjxl.git
   cd libjxl
-  git reset --hard d3a69dbeef78f036969a2500f949f931df857e17
-  git submodule update --init --recursive --depth 1 --recommend-shallow third_party/{highway,libjpeg-turbo}
-  #workaround not support excluding libs
-  execute patch -p1 -i ../../libjxl-fix-exclude-libs.patch
-  
+  #git reset --hard d3a69dbeef78f036969a2500f949f931df857e17
+  git submodule update --init --recursive --depth 1 --recommend-shallow third_party/libjpeg-turbo
+  #workaround unknown option: --exclude-libs=ALL
+  sed -i "" '/Check whether the linker support excluding libs/,+6d' lib/jxl.cmake
+  sed -i "" '/if(LINKER_SUPPORT_EXCLUDE_LIBS)/,+4d' lib/jxl.cmake  
   make_dir out
   cd out || exit  
   execute cmake ../ \
