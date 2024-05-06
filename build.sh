9e7258b6033b97a820098332263d9c058e117a73
eko5624 <562433403@qq.com>
2024-05-06 22:22:28 +0800

    cargp-c rollback to v0.9.31

--- build.sh
+++ build.sh
@@ -591,7 +591,9 @@ if build "libdovi" "main"; then
     export RUSTUP_HOME="${WORKSPACE}"/.rustup
     export CARGO_HOME="${WORKSPACE}"/.cargo
     curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable --target x86_64-apple-darwin --no-modify-path
-    curl -OL https://github.com/lu-zero/cargo-c/releases/latest/download/cargo-c-macos.zip
+    #curl -OL https://github.com/lu-zero/cargo-c/releases/latest/download/cargo-c-macos.zip
+    #unzip cargo-c-macos.zip -d "$WORKSPACE/.rustup/toolchains/stable-x86_64-apple-darwin/bin"
+    curl -OL https://github.com/lu-zero/cargo-c/releases/download/v0.9.31/cargo-c-macos.zip
     unzip cargo-c-macos.zip -d "$WORKSPACE/.rustup/toolchains/stable-x86_64-apple-darwin/bin"
   fi
   if [ ! -d "$WORKSPACE/.rustup" ]; then
